62266f4b812bb5fdec69f8e8b3829ea4c474960a
Emilio-Falcao <emilio.falcao@gmail.com>
2020-10-08 16:01:11 -0300

    Validaçao

--- src/app/pages/cadastro/cadastro.component.ts
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/usuario';
 import{FormGroup, FormControl} from '@angular/forms';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.css']
 })
+
+
+
 export class CadastroComponent implements OnInit {
 formUser: FormGroup;
 
   constructor() { }
-
+  
 //Iniciando o Metodo CreaeForm
   ngOnInit(): void {
     this.creatForm(new User);
@@ -23,13 +27,13 @@ this.formUser = new FormGroup({
   confemail: new FormControl(users.confemail),
   senha: new FormControl(users.senha),
   dataNasc: new FormControl(users.dataNasc),
-sexo: new FormControl(users.sexo),
-inativo: new FormControl(users.inativo)
+  sexo: new FormControl(users.sexo),
+  inativo: new FormControl(users.inativo)
 });
 }
 onSubmit(){
   console.log(this.formUser.value);
 alert("Cadastrado com sucesso");
-resizeTo;
-}
+this.formUser.reset(new User());
+  }
 }
