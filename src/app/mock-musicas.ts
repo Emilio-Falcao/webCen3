224c8e793f61777535222153b75f6d5d27673ffc
Emilio-Falcao <emilio.falcao@gmail.com>
2020-10-08 17:50:18 -0300

    Tela de Cadastro

--- src/app/mock-musicas.ts
+++ src/app/mock-musicas.ts
@@ -2,45 +2,45 @@ import { Lista } from "./lista";
 
 export const PlayLista: Lista[] = [
   {
-    id:'1',
+    id:'1-',
     nome: 'As Mais dos Anos 80',
     audio: '../../../assets/audio/musica5.mpeg',
 
   },
   {
-    id: '2',
+    id: '2-',
     nome: 'Só sucessos',
     audio: '../../../assets/audio/musica3.mpeg',
 
   },
   {
-    id: '3',
+    id: '3-',
     nome: 'Sempre em Frente',
     audio: '../../../assets/audio/musica4.mpeg',
 
   },
   {
-    id: '3',
+    id: '4-',
     nome: 'Hits Anos 80',
     audio: '../../../assets/audio/musica2.mpeg',
 
   },
   {
-    id: '4',
+    id: '5-',
     nome: 'Sucessos Anos 80',
     audio: '../../../assets/audio/musica4.mpeg',
 
   },
   {
-    id: '5',
+    id: '6-',
     nome: 'Balada Anos 80',
     audio: '../../../assets/audio/musica3.mpeg',
 
   },
   {
-    id: '6',
+    id: '7 - ',
     nome: 'Anos 80',
-    audio: '../../../assets/audio/musica.mpeg',
+    audio: '../../../assets/audio/musica5.mpeg',
 
   },
 ];
